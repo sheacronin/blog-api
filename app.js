0321a1759aba97ae7ab53afc8a5b916acbb21780
Shea Cronin <shealcronin@gmail.com>
2022-03-18 11:22:16 -0400

    Prepare for deployment

--- app.js
+++ app.js
@@ -6,17 +6,21 @@ require('./config/passport');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
 
 const app = express();
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
+app.use(compression()); //Compress all routes
 
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
